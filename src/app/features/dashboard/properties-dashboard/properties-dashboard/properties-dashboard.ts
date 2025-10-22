9c707791d0e05366354513bf87d0c0283f97ca2d
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-10-22 14:08:02 -0500

    Feat: Creating Properties dashboard base code with ng code generator

---
+++ src/app/features/dashboard/properties-dashboard/properties-dashboard/properties-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-properties-dashboard',
+  imports: [],
+  templateUrl: './properties-dashboard.html',
+  styleUrl: './properties-dashboard.css'
+})
+export class PropertiesDashboard {
+
+}
