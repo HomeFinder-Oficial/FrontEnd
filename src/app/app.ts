1a031aed3b4fa11a0ff64937606263b6510dae12
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-10-22 00:12:24 -0500

    Feat: Updating base code for app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
+//import { NavbarComponent } from './shared/components/navbar/navbar.component';
+//import { FooterComponent } from "./shared/components/footer/footer.component";
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [CommonModule, RouterOutlet, /*NavbarComponent, FooterComponent,*/],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
+
 export class App {
   protected readonly title = signal('FrontEnd');
 }
