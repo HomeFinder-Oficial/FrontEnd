790408e01dea404d18bb8b0a0122d7c2022d98c8
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-10-25 21:18:25 -0500

    Feat: Creating property filters component with base code

---
+++ src/app/shared/components/property-filters/property-filters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-filters.component',
+  imports: [],
+  templateUrl: './property-filters.component.html',
+  styleUrl: './property-filters.component.css'
+})
+export class PropertyFiltersComponent {
+
+}
