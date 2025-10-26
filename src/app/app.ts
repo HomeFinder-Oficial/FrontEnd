45fecb9f09abb950ec7669b76a0bde040b18f810
sebastian <sebastian.cifuentes0808@gmail.com>
2025-10-26 00:49:14 -0500

    footer v1.0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-//import { NavbarComponent } from './shared/components/navbar/navbar.component';
-//import { FooterComponent } from "./shared/components/footer/footer.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-footer',
+  imports: [NgOptimizedImage, MenubarModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
