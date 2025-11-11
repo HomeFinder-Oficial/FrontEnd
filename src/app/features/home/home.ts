aa7f321a543f14c35eb158fc487d78817b76729b
Sebas <joan.saavedra@correounivalle.edu.co>
2025-11-11 09:30:49 -0500

    fix: Fixing errors on the homepage and adding a search engine

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -41,42 +41,75 @@ export class Home {
 
   constructor(private messageService: MessageService, private confirmationService: ConfirmationService) {}
 
-  properties = [
-    {
-      name: 'Palm Harbor',
-      location: '2699 Green Valley, Highland Lake, FL',
-      price: '$2,095 /month',
-      image: 'https://picsum.photos/400/250?random=1',
-      rooms: 3,
-      baths: 2,
-      size: '57.7',
-      isFavorite: false
-    },
-    {
-      name: 'Beverly Springfield',
-      location: '2821 Lake Sevilla, Palm Harbor, TX',
-      price: '$2,700 /month',
-      image: 'https://picsum.photos/400/250?random=2',
-      rooms: 4,
-      baths: 2,
-      size: '67.5',
-      isFavorite: false
-    },
-    {
-      name: 'Faulkner Ave',
-      location: '909 Woodland St, Michigan, IN',
-      price: '$4,550 /month',
-      image: 'https://picsum.photos/400/250?random=3',
-      rooms: 4,
-      baths: 3,
-      size: '81.0',
-      isFavorite: false
-    }
-  ];
+properties = [
+  {
+    value: '$2,400/month',
+    name: 'St. Crystal',
+    address: '123 Main St, Cityville',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    rooms: 3,
+    baths: 2,
+    area: 500,
+    isPopular: false,
+    isFavorite: false
+  },
+  {
+    value: '$3,000/month',
+    name: 'Ocean View',
+    address: '456 Beach Ave, Seaside',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    rooms: 4,
+    baths: 3,
+    area: 750,
+    isPopular: true,
+    isFavorite: false
+  },
+  {
+    value: '$1,800/month',
+    name: 'Sunny Loft',
+    address: '789 Sun Blvd, Downtown',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    rooms: 2,
+    baths: 1,
+    area: 400,
+    isPopular: false,
+    isFavorite: true
+  },
+  {
+    value: '$2,900/month',
+    name: 'Green Villa',
+    address: '321 Garden St, Suburbia',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    rooms: 5,
+    baths: 3,
+    area: 900,
+    isPopular: true,
+    isFavorite: false
+  },
+  {
+    value: '$2,100/month',
+    name: 'Downtown Flat',
+    address: '654 Center Rd, Cityville',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    rooms: 3,
+    baths: 2,
+    area: 600,
+    isPopular: false,
+    isFavorite: false
+  },
+  {
+    value: '$2,750/month',
+    name: 'Mountain Retreat',
+    address: '987 Hilltop Rd, Highland',
+    image: 'https://primefaces.org/cdn/primeng/images/card-ng.jpg',
+    rooms: 4,
+    baths: 2,
+    area: 800,
+    isPopular: true,
+    isFavorite: false
+  }
+];
 
-  // =========================
-  // Funciones existentes
-  // =========================
 
   showConfirm(property: any) { 
     this.confirmationService.confirm({
