f3c5bdabb8041bee981cb10a24d2ac78124ff3ab
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-11-11 21:52:44 -0500

    Feat: Implementing code for the component

--- src/app/shared/components/capsule-button/capsule-button.ts
+++ src/app/shared/components/capsule-button/capsule-button.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-capsule-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './capsule-button.html',
   styleUrl: './capsule-button.css',
 })
-export class CapsuleButton {
 
+export class CapsuleButton {
+  @Input() isLoggedIn = false;
+  
+  buttons = [
+    { label: 'Option 1', active: false },
+    { label: 'Option 2', active: false },
+    { label: 'Option 3', active: false },
+  ];
+
+  toggleActive(index: number) {
+    this.buttons[index].active = !this.buttons[index].active;
+  }
 }
