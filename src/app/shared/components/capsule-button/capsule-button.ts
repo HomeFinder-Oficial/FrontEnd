c109fc7477c9c9bd2a077a0c192948aab0729343
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-11-11 20:57:48 -0500

    Feat: Creating capsule-button component with ng code generated

---
+++ src/app/shared/components/capsule-button/capsule-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capsule-button',
+  imports: [],
+  templateUrl: './capsule-button.html',
+  styleUrl: './capsule-button.css',
+})
+export class CapsuleButton {
+
+}
