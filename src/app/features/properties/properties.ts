5bf3d8f296b67ae5cfc6dfce0714e044fe423171
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-11-13 09:56:18 -0500

    Feat: Improving properties page structure

--- src/app/features/properties/properties.ts
+++ src/app/features/properties/properties.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+import { CapsuleButton } from '../../shared/components/capsule-button/capsule-button';
+import { PropertiesList } from '../../shared/components/properties-list/properties-list';
+import { PropertyDetail } from '../../shared/components/property-detail/property-detail';
+
 @Component({
   selector: 'app-properties',
-  imports: [],
+  imports: [CapsuleButton, PropertyDetail],
   templateUrl: './properties.html',
   styleUrl: './properties.css',
 })
-export class Properties {
 
+export class Properties {
+  //
 }
